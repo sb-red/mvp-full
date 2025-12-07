02634ca68cfe513b295f191122574282616aa1d8
DongilMin <tasabi0826@gmail.com>
2025-12-07 21:53:46 +0900

    Fix: Modify X-ray SDK

--- backend/middleware/xray.go
+++ backend/middleware/xray.go
@@ -3,6 +3,7 @@ package middleware
 import (
 	"context"
 	"fmt"
+	"log"
 
 	"github.com/aws/aws-xray-sdk-go/xray"
 	"github.com/gofiber/fiber/v2"
@@ -11,18 +12,33 @@ import (
 // XRayMiddleware wraps Fiber requests with AWS X-Ray tracing
 func XRayMiddleware() fiber.Handler {
 	return func(c *fiber.Ctx) error {
+		// Skip tracing for health checks to reduce noise
+		if c.Path() == "/health" {
+			return c.Next()
+		}
+
 		// Create a segment name from the route
-		segmentName := fmt.Sprintf("%s %s", c.Method(), c.Path())
+		segmentName := fmt.Sprintf("softgate-backend")
 
 		// Start a new segment
 		ctx, seg := xray.BeginSegment(context.Background(), segmentName)
-		defer seg.Close(nil)
+		defer func() {
+			if seg != nil {
+				seg.Close(nil)
+			}
+		}()
 
 		// Add HTTP request metadata
-		seg.GetHTTP().GetRequest().Method = c.Method()
-		seg.GetHTTP().GetRequest().URL = c.OriginalURL()
-		seg.GetHTTP().GetRequest().ClientIP = c.IP()
-		seg.GetHTTP().GetRequest().UserAgent = c.Get("User-Agent")
+		if seg.GetHTTP() != nil {
+			seg.GetHTTP().GetRequest().Method = c.Method()
+			seg.GetHTTP().GetRequest().URL = c.OriginalURL()
+			seg.GetHTTP().GetRequest().ClientIP = c.IP()
+			seg.GetHTTP().GetRequest().UserAgent = c.Get("User-Agent")
+		}
+
+		// Add route as annotation
+		seg.AddAnnotation("route", c.Path())
+		seg.AddAnnotation("method", c.Method())
 
 		// Store X-Ray context in Fiber locals for downstream use
 		c.Locals("xray-ctx", ctx)
@@ -32,12 +48,17 @@ func XRayMiddleware() fiber.Handler {
 		err := c.Next()
 
 		// Add HTTP response metadata
-		seg.GetHTTP().GetResponse().Status = c.Response().StatusCode()
+		if seg.GetHTTP() != nil {
+			seg.GetHTTP().GetResponse().Status = c.Response().StatusCode()
+		}
 
 		if err != nil {
 			// Mark segment as error
+			log.Printf("Request error: %v", err)
 			seg.AddError(err)
-			seg.GetHTTP().GetResponse().Status = fiber.StatusInternalServerError
+			if seg.GetHTTP() != nil {
+				seg.GetHTTP().GetResponse().Status = fiber.StatusInternalServerError
+			}
 		}
 
 		return err
